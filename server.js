e959d9e4c7b7e7c261ac2669eb3ff696cc97197b
rahulgupta1103 <rahul.gupta@retvensservices.com>
2024-08-17 20:03:58 +0530

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 // Initialize Express app
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const corsConfig ={
   origin:"*",
